029afaa9d485bb414f37bf33e2c519686a9dacb9
mikexie <mxie622@gmail.com>
2018-11-09 12:41:10 +1300

    # medium level question

---
+++ Two_pointers.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
