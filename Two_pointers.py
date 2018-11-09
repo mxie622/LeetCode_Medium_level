2a7dc32b0d4bd2b245226693ec3018e4844f2638
mikexie <mxie622@gmail.com>
2018-11-10 12:44:58 +1300

    # medium level question

--- Two_pointers.py
+++ Two_pointers.py
@@ -15,3 +15,28 @@ class Solution:
                 nums.append(j)
             count = max(count, len(nums))
         return count
+
+# https://leetcode.com/problems/sort-colors/description/
+
+def sortColors(nums):
+
+    length = len(nums)
+    input = []
+    count = []
+    for i in range(length):
+        if nums[i] == 2:
+            input.append(nums[i])
+        elif nums[i] == 0:
+            input.insert(0, nums[i])
+        else:
+            count.append(1)
+            continue
+    for j in range(len(input)):
+        if input[j] < input[j+1]:
+            input = input[:j+1] + count + input[j+1:]
+            break
+        # else:
+        #     continue
+    return input
+
+print(sortColors(nums=nums))
