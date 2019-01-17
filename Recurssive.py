9ea6f1732862e29cad6c6566b056736885756540
mikexie <mxie622@gmail.com>
2019-01-18 01:25:16 +1300

    # medium level question

---
+++ Recurssive.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/longest-univalue-path/
+input = [5,4,5,1,1,5,5,null,2,null,null,5]
+class Solution(object):
+    def longestUnivaluePath(self, root):
+        self.ans = 0
+
+        def arrow_length(node):
+            if not node: return 0
+            left_length = arrow_length(node.left)
+            right_length = arrow_length(node.right)
+            left_arrow = right_arrow = 0
+            if node.left and node.left.val == node.val:
+                left_arrow = left_length + 1
+            if node.right and node.right.val == node.val:
+                right_arrow = right_length + 1
+            self.ans = max(self.ans, left_arrow + right_arrow)
+            return max(left_arrow, right_arrow)
+
+        arrow_length(root)
+        return self.ans
